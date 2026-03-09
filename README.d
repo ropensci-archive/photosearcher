e4a76f0609f421fa0a1c1306d9a00b5f719c4e51
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-03-09 09:20:51 +0100

    Create README.md

---
+++ README.d
@@ -0,0 +1,5 @@
+# photosearcher
+
+[![Project Status: Unsupported](https://www.repostatus.org/badges/latest/unsupported.svg)](https://www.repostatus.org/#unsupported)
+[![Status at rOpenSci Software Peer Review](https://badges.ropensci.org/754_status.svg)](https://github.com/ropensci/software-review/issues/754)
+                                              
